70d1553a68a127214bc3812ae61dc725dc9a8b23
KutarRohit <104295666+KutarRohit@users.noreply.github.com>
2022-04-24 13:13:23 +0530

    pos

---
+++ application/logs/log-2022-04-24.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-04-24 11:51:29 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'billingci' C:\xampp\htdocs\bill\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-04-24 11:51:29 --> Unable to connect to the database
